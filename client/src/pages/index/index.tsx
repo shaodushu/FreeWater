0d9dcb5e9a309d0b59bc368cf3b9385a1b2eaf92
shaodushu <shaodushu@gmail.com>
2021-09-07 16:50:29 +0800

    style💄:  优化界面样式

--- client/src/pages/index/index.tsx
+++ client/src/pages/index/index.tsx
@@ -1,11 +1,16 @@
 import Taro from '@tarojs/taro';
-import { View } from '@tarojs/components'
+import { Video, View } from '@tarojs/components'
 import { useRequest } from 'ahooks'
 import styles from './index.module.less'
-import { Image, Space } from 'antd-mobile'
+import { Image, Input, Toast } from 'antd-mobile'
 import Button from 'antd-mobile/es/components/button'
 import { spawn } from 'child_process';
+import React, { useEffect, useState } from 'react';
 
+function httpString(s) {
+  const reg = /(https?|http|ftp|file):\/\/[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]/g;
+  return s.match(reg)
+}
 
 const Grid: React.FC = (props) => {
   const style = Object.assign({}, {
@@ -20,7 +25,9 @@ const Grid: React.FC = (props) => {
 }
 
 function Index() {
-  const { data, loading, error } = useRequest("http://localhost:3000/post?url=https://v.douyin.com/dL9BfJK/&type=like", {
+  const [value, setValue] = useState('')
+  const { run, data = [], loading, error } = useRequest(`http://192.168.1.79:3000/video?url=${value}`, {
+    manual: true,
     requestMethod: (param: any) => {
       console.log(param)
       return Taro.request({
@@ -29,13 +36,24 @@ function Index() {
     },
     formatResult: (v) => v.data
   })
+
+  console.log(data)
+  useEffect(() => {
+    Taro.getClipboardData({
+      success: function (res) {
+        console.log(res.data)
+        setValue(httpString(res.data))
+      }
+    })
+  }, [])
+
   console.log(data, loading, error)
   if (error) {
     return <div>{error}</div>
   }
-  if (loading) {
-    return <div>loading...</div>
-  }
+  // if (loading) {
+  //   return <div>loading...</div>
+  // }
   // console.log(<Grid columns={3} gap={8} />)
   return <View className={styles.test}>
     {/* <Image src={demoSrc} width={100} height={100} fit='fill' /> */}
@@ -43,20 +61,82 @@ function Index() {
     <Image src={demoSrc} width={100} height={100} fit='cover' />
     <Image src={demoSrc} width={100} height={100} fit='scale-down' />
     <Image src={demoSrc} width={100} height={100} fit='none' /> */}
-    <Grid  >
+    <Input value={value} placeholder="短链" onChange={v => setValue(v)} />
+    <Button loading={loading} block color='primary' size='large' onClick={() => run()}>提取</Button>
+    {data.length > 0 && <>
+      <Video
+        src={data[0]}
+        controls={true}
+        autoplay={false}
+        // poster='http://misc.aotu.io/booxood/mobile-video/cover_900x500.jpg'
+        initialTime={0}
+        id='video'
+        loop={false}
+        muted={false}
+      />
+      <Button block color='primary' size='large' onClick={() => {
+        Taro.downloadFile({
+          url: data[0], //仅为示例，并非真实的资源
+          success: function (res) {
+            console.log(res)
+            // 只要服务器有响应数据，就会把响应内容写入文件并进入 success 回调，业务需要自行判断是否下载到了想要的内容
+            if (res.statusCode === 200) {
+              Toast.show('下载成功')
+              // Taro.paly({
+              //   filePath: res.tempFilePath
+              // })
+              Taro.saveVideoToPhotosAlbum({
+                filePath: res.tempFilePath,
+                success: async function (res) {
+                  console.log(res.errMsg)
+                  await Taro.showToast({
+                    title: '成功',
+                    icon: 'success',
+                    duration: 2000
+                  })
+                  Taro.hideToast()
+                },
+                fail: err => {
+                  console.log(err)
+                  Taro.showToast({
+                    title: err.errMsg,
+                    icon: 'success',
+                    duration: 2000
+                  })
+                }
+              })
+            }
+          },
+          fail: err => {
+            console.log(err)
+          }
+        })
+      }}>下载</Button>
+    </>}
+
+
+    {/* <Video
+      src='http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400'
+      controls={true}
+      autoplay={false}
+      poster='http://misc.aotu.io/booxood/mobile-video/cover_900x500.jpg'
+      initialTime={0}
+      id='video'
+      loop={false}
+      muted={false}
+    /> */}
+    {/* <Grid  >
       {data.map(item => <div key={item.title} className={styles['grid-item']}>
         <Image width={100} height={100} src={item.cover} />
       </div>)}
-    </Grid>
+    </Grid> */}
     {/* <Grid columns={3} gap={8}>
       {data.map(item => <Grid.Item key={item.title}>
         <Image width={100} height={100} src={item.cover} />
       </Grid.Item>)}
     </Grid> */}
 
-    <Button block color='primary' size='large'>
-      Block Button
-    </Button>
+
     {/* <Space className={styles.img_box}>
       {data.map(item => <div className={styles.img}>
         <h3>{item.title}</h3>
