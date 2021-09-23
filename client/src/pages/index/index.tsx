6c104bfdc664ba9de412e5e913727ace9c1aa6ac
icuteme <1515812124@qq.com>
2021-09-23 19:28:33 +0800

    feat✨:  添加视频保存功能

--- client/src/pages/index/index.tsx
+++ client/src/pages/index/index.tsx
@@ -5,47 +5,78 @@ import styles from './index.module.less'
 import { Search, Image, Grid, Button, Row, Col, Loading } from "@taroify/core"
 import { useEffect, useState } from 'react';
 import { httpString } from '@/utils/index';
+import { request } from '@/utils/request';
+
+async function getVideoUrl<T>(value) {
+  return request<T>(`https://laiceps.cloud/api/v1/video?url=${value}`)
+}
+
+function downloadFile(url: string) {
+  return new Promise((
+    resolve: (v: Taro.downloadFile.FileSuccessCallbackResult) => void,
+    reject: (v: Taro.General.CallbackResult) => void,
+  ) => Taro.downloadFile({
+    url,
+    success: v => resolve(v),
+    fail: v => reject(v)
+  }))
+}
 
 function Index() {
   const [value, setValue] = useState('')
-  const { run, data = [], loading, error } = useRequest(`http://139.155.176.28/api/v1/video?url=${value}`, {
+  const { run, data = [], loading, error } = useRequest(() => getVideoUrl<any[]>(value), {
     manual: true,
-    requestMethod: (param: any) => {
-      console.log(param)
-      return Taro.request({
-        url: param
-      })
-    },
-    formatResult: (v) => v.data
+    // requestMethod: (param: any) => {
+    //   console.log(param)
+    //   return Taro.request({
+    //     url: param
+    //   })
+    // },
+    // formatResult: (v) => {
+    //   console.log(v)
+    //   return v.data
+    // }
   })
 
-  const { data: list = [], loading: loadingA } = useRequest(`http://139.155.176.28/api/v1/post?url=https://v.douyin.com/dMjh5pe/`, {
-    requestMethod: (param: any) => {
-      console.log(param)
-      return Taro.request({
-        url: param
-      })
-    },
-    formatResult: (v) => v.data
-  })
+  // const { data: list = [], loading: loadingA } = useRequest(`https://laiceps.cloud/api/v1/post?url=https://v.douyin.com/dMjh5pe/`, {
+  //   requestMethod: (param: any) => {
+  //     console.log(param)
+  //     return Taro.request({
+  //       url: param
+  //     })
+  //   },
+  //   formatResult: (v) => v.data
+  // })
 
-  console.log(list)
   useEffect(() => {
     Taro.getClipboardData({
       success: function (res) {
-        console.log(res.data)
         setValue(httpString(res.data))
       }
     })
   }, [])
 
-  console.log(data, loading, error)
   if (error) {
     return <div>{error}</div>
   }
-  // if (loading) {
-  //   return <div>loading...</div>
-  // }
+
+  const onDownload = async () => {
+    const { statusCode, tempFilePath } = await downloadFile(data[0].replace('-web', ""))
+
+    if (statusCode === 200) {
+      await Taro.saveVideoToPhotosAlbum({
+        filePath: tempFilePath,
+      })
+
+      await Taro.showToast({
+        title: '成功',
+        icon: 'success',
+        duration: 2000
+      })
+      Taro.hideToast()
+    }
+  }
+
 
   return <View className={styles.index}>
     <Search
@@ -58,6 +89,7 @@ function Index() {
     />
     {data.length > 0 && <>
       <Video
+        style={{ width: '100%' }}
         src={data[0]}
         controls={true}
         autoplay={false}
@@ -67,58 +99,10 @@ function Index() {
         loop={false}
         muted={false}
       />
-      <Button block color='primary' size='large' onClick={() => {
-        Taro.downloadFile({
-          url: data[0], //仅为示例，并非真实的资源
-          success: function (res) {
-            console.log(res)
-            // 只要服务器有响应数据，就会把响应内容写入文件并进入 success 回调，业务需要自行判断是否下载到了想要的内容
-            if (res.statusCode === 200) {
-              // Toast.show('下载成功')
-              // Taro.paly({
-              //   filePath: res.tempFilePath
-              // })
-              Taro.saveVideoToPhotosAlbum({
-                filePath: res.tempFilePath,
-                success: async function (res) {
-                  console.log(res.errMsg)
-                  await Taro.showToast({
-                    title: '成功',
-                    icon: 'success',
-                    duration: 2000
-                  })
-                  Taro.hideToast()
-                },
-                fail: err => {
-                  console.log(err)
-                  Taro.showToast({
-                    title: err.errMsg,
-                    icon: 'success',
-                    duration: 2000
-                  })
-                }
-              })
-            }
-          },
-          fail: err => {
-            console.log(err)
-          }
-        })
-      }}>下载</Button>
+      <Button block color='primary' size='large' onClick={onDownload}>下载</Button>
     </>}
 
-
-    {/* <Video
-      src='http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400'
-      controls={true}
-      autoplay={false}
-      poster='http://misc.aotu.io/booxood/mobile-video/cover_900x500.jpg'
-      initialTime={0}
-      id='video'
-      loop={false}
-      muted={false}
-    /> */}
-    <h3>宝子的作品</h3>
+    {/* <h3>宝子的作品</h3>
     {loadingA ? (
       <Loading size="24px" vertical>加载中...</Loading>
     ) : (
@@ -127,7 +111,7 @@ function Index() {
           <Image className={styles.image} src={item.cover} />
         </Grid.Item>)}
       </Grid>
-    )}
+    )} */}
 
   </View>
 }
